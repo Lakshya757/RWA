ccda63b0159d1e29277c7c54ac20ccf0f4e957fb
Lakshya757 <lakshyars75@gmail.com>
2024-11-11 00:17:13 +0530

    Displayed

--- App.js
+++ App.js
@@ -12,95 +12,102 @@ import axios from "axios";
 
 const shits=[];
 
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title[0]}     {title[1]}           {title[2]}</Text>
+  </View>
+);
+
 export default function App() {
   const [data, setData] = useState();
   const [x, setX] = useState();
 
+  const [master, setMaster] = useState();
+  const [transactions, setTransactions] = useState();
+  const [cmwd, setCmwd] = useState();
+  const [bwfc, setBwfc] = useState()
+
+
   const [sheetName, setsheetName] = useState("Master");
   const [starting, setStarting] = useState("A1");
-  const [ending, setEnding] = useState("A10");
+  const [ending, setEnding] = useState("B10");
 
-  const getData = async () => {
+  const getData = async (sheet,range) => {
     try {
       const res = await fetch(
-        `https://sheets.googleapis.com/v4/spreadsheets/1RPvWaKVwv_haeUYQsxDIDUb8PgRMQiNJtjz4YGL-ICs/values/${sheetName}!${starting}:${ending}?key=AIzaSyAGmHlfjxg8TVfJppBO5ckv3CrG_5hkT9w`
+        `https://sheets.googleapis.com/v4/spreadsheets/1RPvWaKVwv_haeUYQsxDIDUb8PgRMQiNJtjz4YGL-ICs/values/${sheet}${range}?key=AIzaSyAGmHlfjxg8TVfJppBO5ckv3CrG_5hkT9w`
       );
 
       const dataCollected = await res.json();
 
+      switch (sheet) {
+        case "Master":
+            setMaster(dataCollected.values);
 
-      setData(dataCollected);
-      
-      // console.log(data);
-      // console.log(data.values[0]);
+          break;
+        
+        case "Transactions":
+          setTransactions(dataCollected.values);
+        break;
+        
+        case "Customer%20Month%20Wise%20Data":
+          setCmwd(dataCollected)
 
-      for(var i = 0;i<10;i++){
-        console.log(data.values[i])
-        shits[i] = data.values[i]
+        break;
+
+        case "Block%20Wise%20Fund%20Collection":
+          setBwfc(dataCollected)
+        break;
+        default:
+          break;
       }
+
     } catch (error) {
       console.log(error);
     }
   };
 
+
+  useEffect(() => {      
+      getData("Master","");
+      getData("Master","");
+      getData("Transactions","");
+      getData("Transactions","");
+      getData("Customer%20Month%20Wise%20Data","");
+      getData("Customer%20Month%20Wise%20Data","");
+      getData("Block%20Wise%20Fund%20Collection","");
+      getData("Block%20Wise%20Fund%20Collection","");
+      console.log(master);
+      console.log(transactions);
+      console.log(cmwd);
+    }, [])
+  
+
   
   return (
-    <View>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.inputs}
-          placeholder="Sheet Name"
-          value={sheetName}
-          onChangeText={(x) => setsheetName(x)}
-        />
-        <TextInput
-          style={styles.inputs}
-          placeholder="Starting Cell"
-          value={starting}
-          onChangeText={(x) => setStarting(x)}
-        />
-        <TextInput
-          style={styles.inputs}
-          placeholder="Ending Cell"
-          value={ending}
-          onChangeText={(x) => setEnding(x)}
-        />
-      </View>
-      <View style={{alignItems:'center'}}>
-        <TouchableOpacity
-          onPress={() => {
-            getData();
-          }}
-        >
-          <Text>Get Data</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <FlatList
-          data={data}
-          renderItem={({item})=>{
-            <View>
-              <Text>{item}</Text>
-            </View>
-          }}
-        />
-
-        <Text>{shits[1]}</Text>
-          
-      </View>
+    <View style={{marginTop:50}}>
+       <FlatList
+        data={master}
+        renderItem={({item}) => <Item title={item} />}
+        // keyExtractor={item => item.id}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    display:'flex',
-    alignItems:'center'
+  container: {
+    flex: 1,
   },
-  inputs:{
-    marginTop:100,
-    borderWidth:1,
-    width:300
-  }
-
+  item: {
+    backgroundColor: '#3A3A3A',
+    padding: 13,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 15,
+    color:'#AAA'
+  },
 });
